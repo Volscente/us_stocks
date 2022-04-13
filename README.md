3ca38905828f7e1339b07af74db63861d0debc32
Simone Porreca <porrecasimone@gmail.com>
2022-04-13 18:36:17 +0200

    Setup logging module

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Analysis of hundreds of financial indicators for US stocks.
 
 [Dataset](https://www.kaggle.com/datasets/cnic92/200-financial-indicators-of-us-stocks-20142018)
+
+# Installation
+
+## Environment Variable
+Setup an environment variable called "us_stocks_path" with the 
+absolute path to the project directory.
---
+++ modules/logging_module/logging_module.py
@@ -0,0 +1,24 @@
+# Import Standard Modules
+import logging.config
+import yaml
+import os
+
+# Set root path
+os.chdir(os.environ['us_stocks_path'])
+
+
+def get_logger(logger_name):
+    """
+    Set the configuration for the logging module and return the requested logger
+    :return: Logger object
+    """
+
+    # Read the log_configuration file
+    with open('configuration/log_configuration.yaml', 'r') as file:
+        log_config = yaml.safe_load(file.read())
+        logging.config.dictConfig(log_config)
+
+    # Retrieve the requested logger
+    logger = logging.getLogger(logger_name)
+
+    return logger
