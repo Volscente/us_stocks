28d97d51e053f8720f16736c64128e9e843049c6
Simone Porreca <porrecasimone@gmail.com>
2022-04-29 20:50:34 +0200

    test_data fixture

--- modules/pytest_test/test_data_preparation_fixtures.py
+++ modules/pytest_test/test_data_preparation_fixtures.py
@@ -2,6 +2,9 @@
 import pytest
 import yaml
 
+# Import Package Modules
+from modules.data_preparation.data_preparation_utils import load_data
+
 
 @pytest.fixture
 def configuration():
@@ -14,3 +17,19 @@ def configuration():
     configuration = yaml.safe_load(open('./configuration/config.yaml'))
 
     return configuration
+
+
+@pytest.fixture
+def test_data(configuration):
+    """
+    Initiate a Pandas test dataset
+    :return: Pandas test dataset
+    """
+
+    # Read Pandas test dataset
+    data = load_data(configuration['year_start'],
+                     configuration['year_end'],
+                     configuration['data_path'],
+                     configuration['data_filename'])
+
+    return data
