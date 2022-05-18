c3d10bb38d796021cb9ff70e02c4f023c59356c0
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-05-18 23:04:18 +0200

    Updated logging module path

--- modules/logging_module/logging_module.py
+++ modules/logging_module/logging_module.py
@@ -4,17 +4,19 @@ import yaml
 import os
 
 # Set root path
-os.chdir(os.environ['us_stocks_path'])
+# os.chdir(os.environ['us_stocks'])
 
 
-def get_logger(logger_name):
+def get_logger(logger_name: str = None):
     """
     Set the configuration for the logging module and return the requested logger
     :return: Logger object
     """
 
+    directory = os.path.abspath('../configuration/')
+
     # Read the log_configuration file
-    with open('./configuration/log_configuration.yaml', 'r') as file:
+    with open(os.path.join(directory, 'log_configuration.yaml'), 'r') as file:
         log_config = yaml.safe_load(file.read())
         logging.config.dictConfig(log_config)
 
@@ -22,3 +24,7 @@ def get_logger(logger_name):
     logger = logging.getLogger(logger_name)
 
     return logger
+
+
+if __name__ == '__main__':
+    get_logger()
